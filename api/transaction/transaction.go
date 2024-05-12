39fe53ee2c59229418beb6184139251abc49a082
Karn <gitong23@gmail.com>
2024-05-12 09:49:53 +0700

    fixed conflict transaction

--- api/transaction/transaction.go
+++ api/transaction/transaction.go
@@ -11,43 +11,6 @@ type Transaction struct {
 	ImageURL        string  `json:"image_url"`
 }
 
-/*
-{
-	"transactions": [
-		{
-			"id": 1,
-			"date": "2024-04-30T09:00:00.000Z",
-			"amount": 1000,
-			"category": "Food",
-			"transaction_type": "expense",
-			"spender_id": 1,
-			"note": "Lunch",
-			"image_url": "https://example.com/image1.jpg"
-		},
-		{
-			"id": 2,
-			"date": "2024-04-29T19:00:00.000Z",
-			"amount": 2000,
-			"category": "Transport",
-			"transaction_type": "income",
-			"spender_id": 1,
-			"note": "Salary",
-			"image_url": "https://example.com/image2.jpg"
-		}
-	],
-	"summary": {
-		"total_income": 2000,
-		"total_expenses": 1000,
-		"current_balance": 1000
-	},
-	"pagination": {
-		"current_page": 1,
-		"total_pages": 1,
-		"per_page": 10
-	}
-}
-*/
-
 type TransactionSummary struct {
 	TotalIncome    float64 `json:"total_income"`
 	TotalExpenses  float64 `json:"total_expenses"`
@@ -64,4 +27,14 @@ type TransactionWithDetail struct {
 	Transactions []Transaction      `json:"transactions"`
 	Summary      TransactionSummary `json:"summary"`
 	Pagination   PaginationInfo     `json:"pagination"`
+}
+
+type TransactionReqBody struct {
+	Date            string  `json:"date"`
+	Amount          float64 `json:"amount"`
+	Category        string  `json:"category"`
+	TransactionType string  `json:"transaction_type"`
+	SpenderID       int     `json:"spender_id"`
+	Note            string  `json:"note"`
+	ImageURL        string  `json:"image_url"`
 }
