e52dfd7c8e5b72352b4835d77f50ec40342ddeb9
Thitiphum Chaikarnjanakit <thitiphum.dev@gmail.com>
2024-05-12 10:14:18 +0700

    Add get transaction route

--- api/api.go
+++ api/api.go
@@ -33,6 +33,7 @@ func New(db *sql.DB, cfg config.Config, logger *zap.Logger) *Server {
 	v1.GET("/slow", health.Slow)
 	v1.GET("/health", health.Check(db))
 	v1.POST("/upload", eslip.Upload)
+	v1.GET("/transactions", transaction.GetTransactionsHandler(db))
 
 	{
 		h := spender.New(cfg.FeatureFlag, db)
