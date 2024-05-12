f3d63ffc8bf194bcb95ab618e92a521267c06f83
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-05-12 11:42:28 +0700

    test: add TestGetTransactionDetailBySpenderIdWithSQLMock

--- api/transaction/transactionDetail_test.go
+++ api/transaction/transactionDetail_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/KKGo-Software-engineering/workshop-summer/api/config"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
@@ -271,4 +272,75 @@ func (s StubTxDetailStorer) GetTransactionDetailBySpenderId(ctx context.Context,
 
 func (s StubTxDetailStorer) GetTransactionSummaryBySpenderId(ctx context.Context, id string) (TransactionSummary, error) {
 	return s.txSummary, nil
+}
+
+//=================================================================================================
+// SQL Mock
+
+func TestGetTransactionDetailBySpenderIdWithSQLMock(t *testing.T) {
+	t.Run("get transaction detail by spender id", func(t *testing.T) {
+		//create a new echo instance
+		e := echo.New()
+		defer e.Close()
+
+		//create a new http request
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/spenders/1/transactions", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetParamNames("id")
+		c.SetParamValues("1")
+
+		db, mock, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		defer db.Close()
+
+		rows := sqlmock.NewRows([]string{"id", "date", "amount", "category", "transaction_type", "spender_id", "note", "image_url"}).
+			AddRow("1", "2024-04-30T09:00:00.000Z", 1000, "Food", "expense", 1, "Lunch", "https://example.com/image1.jpg").
+			AddRow("2", "2024-04-29T19:00:00.000Z", 2000, "Transport", "income", 1, "Salary", "https://example.com/image2.jpg")
+		mock.ExpectQuery(`SELECT id,date,amount,category, transaction_type,spender_id, note, image_url FROM transaction WHERE spender_id = $1 OFFSET $2 LIMIT $3`).WithArgs("1", 0, 10).WillReturnRows(rows)
+
+		rowCount := sqlmock.NewRows([]string{"count"}).AddRow(2)
+		mock.ExpectQuery(`SELECT COUNT(*) FROM transaction WHERE spender_id = $1`).WithArgs("1").WillReturnRows(rowCount)
+
+		h := New(config.FeatureFlag{}, &Postgres{Db: db})
+		err := h.GetTransactionDetailBySpenderIdHandler(c)
+
+		tmp := rec.Body.String()
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.JSONEq(t, `{
+			"transactions": [
+				{
+					"id": "1",
+					"date": "2024-04-30T09:00:00.000Z",
+					"amount": 1000,
+					"category": "Food",
+					"transaction_type": "expense",
+					"spender_id": 1,
+					"note": "Lunch",
+					"image_url": "https://example.com/image1.jpg"
+				},
+				{
+					"id": "2",
+					"date": "2024-04-29T19:00:00.000Z",
+					"amount": 2000,
+					"category": "Transport",
+					"transaction_type": "income",
+					"spender_id": 1,
+					"note": "Salary",
+					"image_url": "https://example.com/image2.jpg"
+				}
+			],
+			"summary": {
+				"total_income": 2000,
+				"total_expenses": 1000,
+				"current_balance": 1000
+			},
+			"pagination": {
+				"current_page": 1,
+				"total_pages": 1,
+				"per_page": 10
+			}
+		}`, tmp)
+	})
+
 }
